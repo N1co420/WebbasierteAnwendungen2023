5d9450ee8332f2ad147c4dc39137d8bc0c040b34
Nico <nico.huebner@protonmail.com>
2023-05-30 13:14:30 +0200

    added imports

--- Praktikum/src/java/database/TaskGroup.java
+++ Praktikum/src/java/database/TaskGroup.java
@@ -3,6 +3,10 @@ package database;
 import jakarta.persistence.*;
 import jakarta.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 /**
  * Java class to create a TaskGroup
  */
@@ -48,4 +52,90 @@ public class TaskGroup implements Serializable{
     public void setId(long id) {
         this.id = id;
     }
+    public void addToDatabase() {
+        Connection conn = Database.getDBConnection();
+        try {
+            Statement statement = conn.createStatement();
+            this.id = statement.executeUpdate(
+                    "INSERT INTO taskgroup (title, description) " +
+                            "VALUES ('" + title + "', '" + description + "') " +
+                            "RETURNING id");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void deleteFromDatabase() {
+        Connection conn = Database.getDBConnection();
+        try {
+            conn.createStatement().executeUpdate(
+                    "DELETE FROM taskgroup WHERE id = " + this.id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void updateInDatabase() {
+        Connection conn = Database.getDBConnection();
+        try {
+            conn.createStatement().executeUpdate(
+                    "UPDATE taskgroup SET title = '" + title + "', description = '" + description + "' WHERE id = " + this.id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<TaskGroup> getList(){
+        ArrayList<TaskGroup> list = new ArrayList<>();
+        Connection conn = Database.getDBConnection();
+        try {
+            ResultSet rs = conn.createStatement().executeQuery(
+                    "SELECT * FROM taskgroup");
+            while (rs.next()) {
+                list.add(RSToTaskGroup(rs));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    
+    public static TaskGroup getTaskGroupById(int id) {
+        Connection conn = Database.getDBConnection();
+        try {
+            ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM taskgroup WHERE id = " + id);
+            if (rs.next()) {
+                return RSToTaskGroup(rs);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static TaskGroup getTaskGroupByTitle(String title) {
+        Connection conn = Database.getDBConnection();
+        try {
+            ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM taskgroup WHERE title = '" + title + "'");
+            if (rs.next()) {
+                return RSToTaskGroup(rs);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    private static TaskGroup RSToTaskGroup(ResultSet rs) {
+        TaskGroup taskGroup = new TaskGroup();
+        try {
+            taskGroup.setId(rs.getInt("id"));
+            taskGroup.setTitle(rs.getString("title"));
+            taskGroup.setDescription(rs.getString("description"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return taskGroup;
+    }
+    
 }
