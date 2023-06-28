6d929e31e3c135aa54009e5d1e9660fc3a1a0c04
Deedz <dennis.edler@fh-bielefeld.de>
2023-06-28 19:08:52 +0200

    Header Title changed

--- Praktikum/web/script/projekt_aufgabenbereich.js
+++
@@ -1,8 +0,0 @@
-export default class Projekt_Aufgabenbereich {
-
-    constructor(id, projektId, aufgabenbereichId) {
-        this.id = id;
-        this.projektId = projektId;
-        this.aufgabenbereichId = aufgabenbereichId;
-    }
-}
